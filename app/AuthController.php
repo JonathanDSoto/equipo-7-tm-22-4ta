30905b28c4af610941a89b14e0827bb076bdfd4a
aaronsando <111078817+aaronsando@users.noreply.github.com>
2022-10-14 14:12:12 -0600

    Controlador config y auth hecho

--- app/AuthController.php
+++ app/AuthController.php
@@ -2,22 +2,31 @@
 include_once "config.php";
 
 if (isset($_POST['action'])) {
-
+	
 	if ( isset($_POST['global_token']) && 
 		$_POST['global_token'] == $_SESSION['global_token']) {
 
 		switch ($_POST['action']) {
 			case 'access':
 				
+				if (isset($_POST['email']) && isset($_POST['password'])) {
+					
+					$authController = new AuthController();
+
+					$email = strip_tags($_POST['email']);
+					$password = strip_tags($_POST['password']);
+					$authController->login($email,$password);
+				}
+
+				break;
+			case 'exit':
+
 				$authController = new AuthController();
-
-
+				
 				$email = strip_tags($_POST['email']);
-				$password = strip_tags($_POST['password']);
+				$authController -> logout($email);
 
-				$authController->login($email,$password);
-
-			break; 
+				break; 
 		}
 
 	}
@@ -26,8 +35,7 @@ if (isset($_POST['action'])) {
 
 Class AuthController{
 
-	public function login($email,$password)
-	{
+	public function login($email,$password){
 
 		$curl = curl_init();
 
@@ -61,22 +69,44 @@ Class AuthController{
 			header("Location:".BASE_PATH."products");
 		}else{
 
-
 			#var_dump($response);
 			header("Location:".BASE_PATH."?error=true");
 		}
 
 	}
+
+	public function logout($email){
+
+		$curl = curl_init();
+
+		curl_setopt_array($curl, array(
+		CURLOPT_URL => 'https://crud.jonathansoto.mx/api/logout',
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_ENCODING => '',
+		CURLOPT_MAXREDIRS => 10,
+		CURLOPT_TIMEOUT => 0,
+		CURLOPT_FOLLOWLOCATION => true,
+		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		CURLOPT_CUSTOMREQUEST => 'POST',
+		CURLOPT_POSTFIELDS => array(
+			'email' => $email,
+			),
+		));
+
+		$response = curl_exec($curl);
+		curl_close($curl);
+		
+		if ( isset($response->code) && $response->code > 0) {
+
+			header("Location:".BASE_PATH."products");
+		}else{
+
+			#var_dump($response);
+			header("Location:".BASE_PATH."?error=true");
+		}
+	}
+
 }
-
-
-
-
-
-
-
-
-
 
 
 ?>
--- app/config.php
+++ app/config.php
@@ -5,7 +5,13 @@
 		$_SESSION['global_token'] = md5( uniqid( mt_rand(),true ) );
 	}
 
+	$user_agent = getenv("HTTP_USER_PATH");
+
 	if (!defined('BASE_PATH')) {
-		define('BASE_PATH','http://localhost/equipo-7-tm-22-4ta/');
+		if (strpos($user_agent, "Win") !== FALSE) {
+			define('BASE_PATH','http://localhost/equipo-7-tm-22-4ta/');
+		}elseif (strpos($user_agent, "Mac") !== FALSE) {
+			define('BASE_PATH','http://localhost:8888/equipo-7-tm-22-4ta/');
+		}
 	}
 ?>
