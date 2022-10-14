999a9e8698f2e4e46202a7268e4db287e6a1a3df
aaronsando <111078817+aaronsando@users.noreply.github.com>
2022-10-14 17:48:00 -0600

    Se elimino validacion de SO

--- app/config.php
+++ app/config.php
@@ -8,10 +8,7 @@
 	$user_agent = getenv("HTTP_USER_PATH");
 
 	if (!defined('BASE_PATH')) {
-		if (strpos($user_agent, "Win") !== FALSE) {
-			define('BASE_PATH','http://localhost/equipo-7-tm-22-4ta/');
-		}elseif (strpos($user_agent, "Mac") !== FALSE) {
-			define('BASE_PATH','http://localhost:8888/equipo-7-tm-22-4ta/');
-		}
+		define('BASE_PATH','http://localhost/equipo-7-tm-22-4ta/');
 	}
+	
 ?>
