9017f8b1f349e074ca61dd7eb820ac8999d5e167
EridaniLC <110060486+EridaniLC@users.noreply.github.com>
2022-10-20 01:01:25 -0600

    crear productos listo

--- products/add-products.php
+++ products/add-products.php
@@ -61,192 +61,91 @@
 
                     <form id="createproduct-form" autocomplete="off" class="needs-validation" novalidate>
                         <div class="row">
-                            <div class="col-lg-4">
+                            <div class="col-lg-6"> 
                                 <div class="card">
+                                    <div class="card-header">
+                                        <h5 class="card-title mb-0">Añadir Producto</h5>
+                                    </div>
                                     <div class="card-body">
                                         <div class="mb-3">
-                                            <label class="form-label" for="product-title-input">Titulo del Producto</label>
+                                            <label class="form-label" for="product-title-input">Titulo del producto</label>
                                             <input type="hidden" class="form-control" id="formAction" name="formAction" value="add">
                                             <input type="text" class="form-control d-none" id="product-id-input">
-                                            <input type="text" class="form-control" id="product-title-input" value="" placeholder="Introduce el titulo del producto" required>
-                                            <div class="invalid-feedback">Por favor Introduzca un título de producto.</div>
+                                            <input type="text" class="form-control" id="product-title-input" value="" placeholder="Ingrese el titulo del producto" required>
+                                            <div class="invalid-feedback">Ingrese un título de producto.</div>
+                                        </div>
+
+                                        <div class="mb-3">
+                                            <label class="form-label" for="product-title-input">Descripción general del producto</label>
+                                            <textarea class="form-control" placeholder="Debe ingresar un mínimo de 100 caracteres" rows="3"></textarea>
                                         </div>
                                         
+                                        <div class="mb-3">
+                                            <label class="form-label" for="product-title-input">Categorías de Producto</label>
+                                            <a href="#" class="float-end text-decoration-underline">Agregar nuevo</a>
+                                            
+                                            <select class="form-select" id="choices-category-input" name="choices-category-input" data-choices data-choices-search-false>
+                                                <option value="Appliances">Accesorios</option>
+                                                <option value="Automotive Accessories">Accesorios automotrices</option>
+                                                <option value="Electronics">Electrónica</option>
+                                                <option value="Fashion">Moda</option>
+                                                <option value="Furniture">Muebles</option>
+                                                <option value="Grocery">Comestibles</option>
+                                                <option value="Kids">Niños</option>
+                                                <option value="Watches">Relojes</option>
+                                            </select>
+                                        </div>
+                                        <div class="mb-3">
+                                            <label class="form-label" for="product-title-input">Etiquetas de productos</label>                                            
+                                            <div class="hstack gap-3 align-items-start">
+                                                <div class="flex-grow-1">
+                                                    <input class="form-control" data-choices data-choices-multiple-remove="true" placeholder="Ingrese una etiqueta" type="text" value="Cotton"/>
+                                                </div>
+                                            </div>
+                                        
+                                        
+                                        </div>
+
                                     </div>
-                                </div> 
-                                <!-- end card -->
- 
+                                </div>
+                            </div>
+                            <!-- end col -->
+                            <div class="col-lg-6"> 
                                 <div class="card">
                                     <div class="card-header">
-                                        <h5 class="card-title mb-0">Publish</h5>
+                                        <h5 class="card-title mb-0">Detalles del Producto</h5>
                                     </div>
                                     <div class="card-body">
                                         <div class="mb-3">
-                                            <label for="choices-publish-status-input" class="form-label">Status</label>
-
-                                            <select class="form-select" id="choices-publish-status-input" data-choices data-choices-search-false>
-                                                <option value="Published" selected>Published</option>
-                                                <option value="Scheduled">Scheduled</option>
-                                                <option value="Draft">Draft</option>
-                                            </select>
+                                            <label class="form-label" for="product-title-input">Características principales</label>
+                                            <textarea class="form-control" placeholder="Debe ingresar un mínimo de 100 caracteres" rows="3"></textarea>
                                         </div>
-
-                                        <div>
-                                            <label for="choices-publish-visibility-input" class="form-label">Visibility</label>
-                                            <select class="form-select" id="choices-publish-visibility-input" data-choices data-choices-search-false>
-                                                <option value="Public" selected>Public</option>
-                                                <option value="Hidden">Hidden</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                    <!-- end card body -->
-                                </div>
-                                
-                            </div>
-                            <!-- end col -->
-
-                            <div class="col-lg-4">
-                                
-                                <!-- end card -->
-
-                                <div class="card">
-                                    <div class="card-header">
-                                        <h5 class="card-title mb-0">Publish Schedule</h5>
-                                    </div>
-                                    <!-- end card body -->
-                                    <div class="card-body">
-                                        <div>
-                                            <label for="datepicker-publish-input" class="form-label">Publish Date & Time</label>
-                                            <input type="text" id="datepicker-publish-input" class="form-control" placeholder="Enter publish date" data-provider="flatpickr" data-date-format="d.m.y" data-enable-time>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!-- end card -->
-
-                                <div class="card">
-                                    <div class="card-header">
-                                        <h5 class="card-title mb-0">Product Categories</h5>
-                                    </div>
-                                    <div class="card-body">
-                                        <p class="text-muted mb-2"> <a href="#" class="float-end text-decoration-underline">Add
-                                                New</a>Select product category</p>
-                                        <select class="form-select" id="choices-category-input" name="choices-category-input" data-choices data-choices-search-false>
-                                            <option value="Appliances">Appliances</option>
-                                            <option value="Automotive Accessories">Automotive Accessories</option>
-                                            <option value="Electronics">Electronics</option>
-                                            <option value="Fashion">Fashion</option>
-                                            <option value="Furniture">Furniture</option>
-                                            <option value="Grocery">Grocery</option>
-                                            <option value="Kids">Kids</option>
-                                            <option value="Watches">Watches</option>
-                                        </select>
-                                    </div>
-                                    <!-- end card body -->
-                                </div>
-                                <!-- end card -->
-                                <div class="card">
-                                    <div class="card-header">
-                                        <h5 class="card-title mb-0">Product Tags</h5>
-                                    </div>
-                                    <div class="card-body">
-                                        <div class="hstack gap-3 align-items-start">
-                                            <div class="flex-grow-1">
-                                                <input class="form-control" data-choices data-choices-multiple-remove="true" placeholder="Enter tags" type="text" value="Cotton" />
+                                        
+                                        <div class="mb-3">
+                                            <label class="form-label" for="product-title-input">Marca del producto</label>                                            
+                                            <div class="hstack gap-3 align-items-start">
+                                                <div class="flex-grow-1">
+                                                    <input class="form-control" data-choices data-choices-multiple-remove="true" placeholder="Ingrese una marca" type="text" value="Cotton"/>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                    <!-- end card body -->
-                                </div>
-                                <!-- end card -->
-
-                                <div class="card">
-                                    <div class="card-header">
-                                        <h5 class="card-title mb-0">Product Short Description</h5>
-                                    </div>
-                                    <div class="card-body">
-                                        <p class="text-muted mb-2">Add short description for product</p>
-                                        <textarea class="form-control" placeholder="Must enter minimum of a 100 characters" rows="3"></textarea>
-                                    </div>
-                                    <!-- end card body -->
-                                </div>
-                                <!-- end card -->
-
-                            </div>
-                            <!-- end col -->
-                            <div class="col-lg-4">
-                                
-                                <!-- end card -->
-
-                                <div class="card">
-                                    <div class="card-header">
-                                        <h5 class="card-title mb-0">Publish Schedule</h5>
-                                    </div>
-                                    <!-- end card body -->
-                                    <div class="card-body">
-                                        <div>
-                                            <label for="datepicker-publish-input" class="form-label">Publish Date & Time</label>
-                                            <input type="text" id="datepicker-publish-input" class="form-control" placeholder="Enter publish date" data-provider="flatpickr" data-date-format="d.m.y" data-enable-time>
+                                        
+                                        <div class="mb-3">
+                                            <label class="form-label" for="product-title-input">Palabra clave del producto</label>
+                                            <input type="hidden" class="form-control" id="formAction" name="formAction" value="add">
+                                            <input type="text" class="form-control d-none" id="product-id-input">
+                                            <input type="text" class="form-control" id="product-title-input" value="" placeholder="Ingrese el titulo del producto" required>
+                                            <div class="invalid-feedback">Ingrese la palabra del producto.</div>
                                         </div>
                                     </div>
                                 </div>
-                                <!-- end card -->
-
-                                <div class="card">
-                                    <div class="card-header">
-                                        <h5 class="card-title mb-0">Product Categories</h5>
-                                    </div>
-                                    <div class="card-body">
-                                        <p class="text-muted mb-2"> <a href="#" class="float-end text-decoration-underline">Add
-                                                New</a>Select product category</p>
-                                        <select class="form-select" id="choices-category-input" name="choices-category-input" data-choices data-choices-search-false>
-                                            <option value="Appliances">Appliances</option>
-                                            <option value="Automotive Accessories">Automotive Accessories</option>
-                                            <option value="Electronics">Electronics</option>
-                                            <option value="Fashion">Fashion</option>
-                                            <option value="Furniture">Furniture</option>
-                                            <option value="Grocery">Grocery</option>
-                                            <option value="Kids">Kids</option>
-                                            <option value="Watches">Watches</option>
-                                        </select>
-                                    </div>
-                                    <!-- end card body -->
-                                </div>
-                                <!-- end card -->
-                                <div class="card">
-                                    <div class="card-header">
-                                        <h5 class="card-title mb-0">Product Tags</h5>
-                                    </div>
-                                    <div class="card-body">
-                                        <div class="hstack gap-3 align-items-start">
-                                            <div class="flex-grow-1">
-                                                <input class="form-control" data-choices data-choices-multiple-remove="true" placeholder="Enter tags" type="text" value="Cotton" />
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <!-- end card body -->
-                                </div>
-                                <!-- end card -->
-
-                                <div class="card">
-                                    <div class="card-header">
-                                        <h5 class="card-title mb-0">Product Short Description</h5>
-                                    </div>
-                                    <div class="card-body">
-                                        <p class="text-muted mb-2">Add short description for product</p>
-                                        <textarea class="form-control" placeholder="Must enter minimum of a 100 characters" rows="3"></textarea>
-                                    </div>
-                                    <!-- end card body -->
-                                </div>
-                                <!-- end card -->
                                 <div class="text-end mb-3">
-                                    <button type="submit" class="btn btn-success w-sm">Enviar</button>
+                                        <button type="submit" class="btn btn-success w-lg">Enviar</button>
                                 </div>
                             </div>
                         </div>
                         <!-- end row -->
-
                     </form>
-
                 </div>
                 <!-- container-fluid -->
             </div>
