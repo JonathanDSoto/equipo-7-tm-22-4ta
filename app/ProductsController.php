8aa9cc329bf21bccbfd2cdeb32e3829ac8b5299e
aaronsando <111078817+aaronsando@users.noreply.github.com>
2022-10-25 02:11:25 -0600

    controlador productos completo

--- app/ProductsController.php
+++ app/ProductsController.php
@@ -1,4 +1,5 @@
 <?php  
+
 include_once "config.php";
 
 if (isset($_POST['action'])) {
@@ -139,6 +140,36 @@ Class ProductsController{
 			return array();
 		}
 
+	}
+
+	public function getProductByCategory($slug){
+		$curl = curl_init();
+
+		curl_setopt_array($curl, array(
+		CURLOPT_URL => 'https://crud.jonathansoto.mx/api/products/categories/'.$slug,
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_ENCODING => '',
+		CURLOPT_MAXREDIRS => 10,
+		CURLOPT_TIMEOUT => 0,
+		CURLOPT_FOLLOWLOCATION => true,
+		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		CURLOPT_CUSTOMREQUEST => 'GET',
+		CURLOPT_HTTPHEADER => array(
+			'Authorization: Bearer '.$_SESSION['token']
+		),
+		));
+
+		$response = curl_exec($curl);
+		curl_close($curl);
+		echo $response;
+
+		if ( isset($response->code) && $response->code > 0) {
+			
+			return $response->data;
+		}else{
+
+			return array();
+		}
 	}
 
 	public function createProduct($name,$slug,$description,$features,$brand_id, $cover){
--- app/UserController.php
+++ app/UserController.php
@@ -32,7 +32,7 @@ if (isset($_POST['action'])) {
                 echo json_encode($userController->delete($_POST['id']));
 
 				break;
-            case 'create'
+            case 'create':
 
                 $name = strip_tags($_POST['name']);
                 $lastname = strip_tags($_POST['lastname']);
@@ -238,6 +238,36 @@ Class UserController{
         //     header("Location:".BASE_PATH."products/?create=false")
         // }
 
+    }
+
+    public function updateImage($id, $profile_photo_file){
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://crud.jonathansoto.mx/api/users/avatar',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS => array('id' => $id,
+        'profile_photo_file'=> new CURLFILE($profile_photo_file)),
+        CURLOPT_HTTPHEADER => array(
+            'Authorization: Bearer '.$_SESSION['token']
+        ),
+        ));
+
+        $response = curl_exec($curl);
+        curl_close($curl);
+        echo $response;
+
+        if(isset($response->code) && $response->code >0 ){
+            header("Location:".BASE_PATH."products/?success=true");
+        }else {
+            header("Location:".BASE_PATH."products/?error=true");
+        }
     }
 
 
